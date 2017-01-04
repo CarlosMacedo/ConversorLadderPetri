bc8d05c6f5cb559ef3ebadcca7e7d2a47adfeeb2
Carlos Macêdo <carlosmacedo025@gmail.com>
2017-01-04 00:43:02 -0200

    License

--- ConversorLadderPetri/src/main/Main.java
+++ ConversorLadderPetri/src/main/Main.java
@@ -1,10 +1,43 @@
+/**
+ * Copyright 2017 Carlos Macêdo
+ * 
+ * This file is part of ConversorLadderPetri software.
+ * 
+ *  ConversorLadderPetri is free software: you can 
+ *  redistribute it and/or modify it under the terms of the GNU General 
+ *  Public License as published by the Free Software Foundation, either 
+ *  version 3 of the License, or (at your option) any later version.
+ *  
+ *  ConversorLadderPetri is distributed in the hope that
+ *  it will be useful, but WITHOUT ANY WARRANTY; without even the implied 
+ *  warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See 
+ *  the GNU General Public License for more details.
+ *  You should have received a copy of the GNU General Public License
+ *  along with ConversorLadderPetri. 
+ *  If not, see <http://www.gnu.org/licenses/>.
+ *  
+ *  Contact: carlosmacedo025 at gmail dot com
+ */
 package main;
 
 import java.io.IOException;
 import javax.swing.JFileChooser;
 
+/**
+ * Converts a Ladder file, generated in LDmicro,
+ * in a .cpn file for the CPNTools 4.0.1.
+ * 
+ * @author Carlos Macedo
+ */
 
 public class Main {
+	
+	/**
+	 * Select file, convert and write.
+	 * 
+	 * @param args 
+	 * @throws IOException Error reading file.
+	 */
 	public static void main(String[] args) throws IOException {
 		JFileChooser fc = new JFileChooser();
         fc.showOpenDialog(null);
@@ -18,6 +51,13 @@ public class Main {
 		fml.ladderToPetri(path, out);
 	}
 	
+	/**
+	 * Finds from which directory the .ld file was selected.
+	 * That is, it just removes the file name from the end of the string.
+	 *  
+	 * @param path Absolute path of the .ld file. Including the filename.
+	 * @return Returns the absolute path, now without the name of the .ld file.
+	 */
 	private static String getOut(String path) {
 		int index = 0;
 		for (int i = path.length()-1; i >=0 ; i--) {
@@ -30,6 +70,14 @@ public class Main {
 		
 		return extractOut(path, index);
 	}
+	
+	/**
+	 * Returns the absolute path, now with the name of the .cpn file.
+	 * 
+	 * @param path Path obtained by the getOut function.
+	 * @param index Number of characters in the path.
+	 * @return Returns the absolute path, now with the name of the .cpn file.
+	 */
 	private static String extractOut(String path, int index) {
 		String result = "out.cpn";
 		for (int i = index; i >= 0; i--) {
